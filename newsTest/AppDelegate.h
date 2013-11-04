5e7514dda4d3055628105870ac3edcb74092f89d
zhaoch86 <jeromezh@JeromeZHtekiMac-mini.local>
2013-11-04 11:35:03 +0800

    Initial Commit

---
+++ newsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  newsTest
+//
+//  Created by Andy.zhao on 13-11-4.
+//  Copyright (c) 2013年 hzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ newsTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  newsTest
+//
+//  Created by Andy.zhao on 13-11-4.
+//  Copyright (c) 2013年 hzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
